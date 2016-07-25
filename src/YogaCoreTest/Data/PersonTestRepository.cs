73c54512f675211633fc653fd38353209f2a9ef1
I Putu Yoga Permana <putuyoga@outlook.com>
2016-07-25 17:41:49 +0800

    add sample to test PersonController

---
+++ src/YogaCoreTest/Data/PersonTestRepository.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YogaCore.Data;
+using YogaCore.Models;
+
+namespace YogaCoreTest.Data
+{
+    public class PersonTestRepository : IPersonRepository
+    {
+        private List<Person> _listPerson;
+
+        public PersonTestRepository(List<Person> listPerson)
+        {
+            _listPerson = listPerson;
+        }
+
+        public async Task<Person> Create(Person item, string password)
+        {
+            _listPerson.Add(item);
+            return await Task.Run(()=> { return item; });
+        }
+
+        public void Delete(Person item)
+        {
+            _listPerson.Remove(item);
+        }
+
+        public IEnumerable<Person> Get()
+        {
+            return _listPerson.ToList();
+        }
+
+        public Person Get(string id)
+        {
+            return _listPerson.Where(person => person.Id == id).FirstOrDefault();
+        }
+
+        public Person Update(Person item)
+        {
+            var selected = _listPerson.Where(person => person.Id == item.Id).FirstOrDefault();
+            if (selected != null)
+            {
+                selected.BirthDate = item.BirthDate;
+                selected.FirstName = item.FirstName;
+                selected.Email = item.Email;
+                selected.Sex = item.Sex;
+                return selected;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
--- src/YogaCoreTest/TestPersonController.cs
+++ src/YogaCoreTest/TestPersonController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
+using YogaCore.Controllers.API;
 using YogaCore.Models;
+using YogaCoreTest.Data;
 
 namespace YogaCoreTest
 {
@@ -17,8 +19,21 @@ namespace YogaCoreTest
         [Fact]
         public void GetAllPerson_ShouldReturnAllPerson()
         {
-        
+            // create sample data
+            var listPerson = new List<Person>()
+            {
+                new Person() {FirstName = "A" },
+                new Person() {FirstName = "B" },
+                new Person() {FirstName = "B" },
+                new Person() {FirstName = "C" },
+            };
+
+            var repository = new PersonTestRepository(listPerson);
+            var controller = new PersonController(repository);
+
+            Assert.Equal(4, controller.Get().Count());
         }
+
 
     }
 }

7602d34ce6c6ffeec0b5283f844e6305032e1efe
I Putu Yoga Permana <putuyoga@outlook.com>
2016-07-25 17:42:53 +0800

    Change to implement view of some partial json data property

---
+++ src/YogaCore/ViewModels/API/PersonJsonData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YogaCore.Models;
+
+namespace YogaCore.ViewModels.API
+{
+    public class PersonJsonData
+    {
+        public string Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public Sex Sex { get; set; }
+    }
+}

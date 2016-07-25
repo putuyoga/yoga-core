9582a25b0e59a5ff907bb2e7e4c57c6530b8c634
I Putu Yoga Permana <putuyoga@outlook.com>
2016-07-25 17:35:08 +0800

    add sample

---
+++ src/YogaCore/Extensions/PersonSample.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YogaCore.Models;
+
+namespace YogaCore.Extensions
+{
+    public static class PersonSample
+    {
+        public static async Task LoadSample(this UserManager<Person> userManager)
+        {
+            var email = "putuyoga@outlook.com";
+            var role = "Administrator";
+            var existPerson = await userManager.FindByEmailAsync(email);
+
+            // this person did not exist, so just create them anyway
+            if (existPerson == null)
+            {
+                var person = new Person()
+                {
+                    FirstName = "I Putu Yoga Permana",
+                    BirthDate = DateTime.Now,
+                    Sex = Sex.Man,
+                    Email = email
+                };
+                var result = await userManager.CreateAsync(person, "putuyoga@GMAIL.com");
+                var newPerson = await userManager.FindByEmailAsync(email);
+
+                // assign role
+                await userManager.AddToRoleAsync(newPerson, role);
+            }
+            else // otherwise check role, applied to administrator
+            {
+                var listRole = await userManager.GetRolesAsync(existPerson);
+                if(!listRole.Contains(role))
+                {
+                    await userManager.AddToRoleAsync(existPerson, role);
+                }
+            }
+        }
+    }
+}
---
+++ src/YogaCore/Extensions/RoleSample.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YogaCore.Extensions
+{
+    public static class RoleSample
+    {
+        private static async Task EnsureRoleCreated(RoleManager<IdentityRole> roleManager, string roleName)
+        {
+            if (!await roleManager.RoleExistsAsync(roleName))
+            {
+                await roleManager.CreateAsync(new IdentityRole(roleName));
+            }
+        }
+        public static async Task LoadSample(this RoleManager<IdentityRole> roleManager)
+        {
+            // add all roles, that should be in database, here
+            await EnsureRoleCreated(roleManager, "Banned");
+            await EnsureRoleCreated(roleManager, "Administrator");
+        }
+    }
+}

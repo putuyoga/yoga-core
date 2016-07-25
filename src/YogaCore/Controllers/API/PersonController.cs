a1ec83fe3d20e563036d5370f3ab87ed12f9253a
I Putu Yoga Permana <putuyoga@outlook.com>
2016-07-25 17:43:22 +0800

    change constructor, to support dependency injection.

--- src/YogaCore/Controllers/API/PersonController.cs
+++ src/YogaCore/Controllers/API/PersonController.cs
@@ -4,6 +4,12 @@ using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using YogaCore.Models;
 using Microsoft.EntityFrameworkCore;
+using YogaCore.ViewModels.API;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using YogaCore.Data;
 
 // For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,100 +18,114 @@ namespace YogaCore.Controllers.API
     [Route("api/[controller]")]
     public class PersonController : Controller
     {
-        private MatchContext _context;
+        private IPersonRepository _repository;
 
-        public PersonController(MatchContext context)
+        public PersonController(IPersonRepository repository)
         {
-            _context = context;
-            //System.Threading.Thread.Sleep(5000);
+            _repository = repository;
         }
 
         // GET: api/person
         [HttpGet]
-        public IEnumerable<Person> Get()
+        public IEnumerable<PersonJsonData> Get()
         {
-            return _context.Persons.ToList();
-        }
-
-        // GET: api/person/man
-        [HttpGet("man")]
-        public IEnumerable<Person> GetBySexMan()
-        {
-            return _context.Persons
-                .Where(person => person.Sex == Sex.Man).ToList();
-        }
-
-        // GET: api/person/woman
-        [HttpGet("woman")]
-        public IEnumerable<Person> GetBySexWoman()
-        {
-            return _context.Persons
-                .Where(person => person.Sex == Sex.Woman).ToList();
+            return _repository.Get()
+                .Select(person => new PersonJsonData()
+                {
+                    Id = person.Id,
+                    FirstName = person.FirstName,
+                    BirthDate = person.BirthDate,
+                    Sex = person.Sex
+                }).ToList();
         }
 
         // GET api/person/5
         [HttpGet("{id}")]
         public IActionResult Get(string id)
         {
-            var personEntity = _context.Persons.FirstOrDefault(person => person.Id == id);
-            if(personEntity == null)
+            var personEntity = _repository.Get(id);
+
+            // Check if that entity is exist
+            if (personEntity == null)
             {
                 return NotFound();
             }
-            return new ObjectResult(personEntity);
+
+            // Bundle & expose only relevant properties
+            var jsonData = new PersonJsonData()
+            {
+                Id = personEntity.Id,
+                FirstName = personEntity.FirstName,
+                Sex = personEntity.Sex,
+                BirthDate = personEntity.BirthDate
+            };
+            return new ObjectResult(jsonData);
         }
 
         // POST api/values
+        [Authorize(Roles = "Administrator")]
         [HttpPost]
-        public IActionResult Post([FromBody]Person value)
+        public async Task<IActionResult> Post([FromBody]PersonJsonData value)
         {
             if(ModelState.IsValid)
             {
-                // Assign the new guid the newly created item
-                value.Id = Guid.NewGuid().ToString();
+                var randomEmail = Guid.NewGuid().ToString() + "@changethis.com";
+                var user = new Person
+                {
+                    UserName = randomEmail,
+                    Email = randomEmail,
+                    BirthDate = value.BirthDate,
+                    Sex = value.Sex,
+                    FirstName = value.FirstName
+                };
 
-                _context.Persons.Add(value);
-                _context.SaveChanges();
-                return CreatedAtAction("Get", value.Id);
+                await _repository.Create(user, "!weAs12");
+                return CreatedAtAction("Get", user.Id);
             }
             return BadRequest();
         }
 
         // PUT api/person/5
+        [Authorize(Roles = "Administrator")]
         [HttpPut("{id}")]
-        public IActionResult Put(string id, [FromBody]Person value)
+        public IActionResult Put(string id, [FromBody]PersonJsonData value)
         {
-            // Make sure assigned
-            value.Id = id;
-
-            _context.Persons.Attach(value);
-            var entry = _context.Entry(value);
-            entry.State = EntityState.Modified;
-
-            try
+            if (ModelState.IsValid)
             {
-                _context.SaveChanges();
-            }
-            catch (DbUpdateConcurrencyException ex)
-            {
-                return NotFound();
-            }
+                Person entity = _repository.Get(id);
+                if (entity == null)
+                {
+                    return NotFound();
+                }
+                
+                // Update the selected value
+                entity.FirstName = value.FirstName;
+                entity.BirthDate = value.BirthDate;
+                entity.Sex = value.Sex;
 
-            return NoContent();
+                _repository.Update(entity);
+
+                return NoContent();
+            }
+            else
+            {
+                return BadRequest();
+            }
             
         }
-
         // DELETE api/person/5
+        [Authorize(Roles = "Administrator")]
         [HttpDelete("{id}")]
         public IActionResult Delete(string id)
         {
-            var personEntity = _context.Persons.FirstOrDefault(person => person.Id == id);
+            var personEntity = _repository.Get(id);
             if(personEntity == null)
             {
                 return BadRequest();
             }
-            _context.Remove(personEntity);
-            _context.SaveChanges();
+
+            _repository.Delete(personEntity);
+
             return NoContent();
         }
     }

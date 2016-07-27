585fed4f45be266237278a0631229fd186f458d8
I Putu Yoga Permana <putuyoga@outlook.com>
2016-07-27 13:49:28 +0800

    Add comment and clean up

--- src/YogaCore/Extensions/PersonSample.cs
+++ src/YogaCore/Extensions/PersonSample.cs
@@ -1,8 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using YogaCore.Models;
 
@@ -10,8 +7,14 @@ namespace YogaCore.Extensions
 {
     public static class PersonSample
     {
+        /// <summary>
+        /// Create some sample person data
+        /// </summary>
+        /// <param name="userManager">Instance that do some basic operation for user</param>
+        /// <returns></returns>
         public static async Task LoadSample(this UserManager<Person> userManager)
         {
+            // Look up the person first
             var email = "putuyoga@outlook.com";
             var role = "Administrator";
             var existPerson = await userManager.FindByEmailAsync(email);
@@ -26,6 +29,8 @@ namespace YogaCore.Extensions
                     Sex = Sex.Man,
                     Email = email
                 };
+
+                // create person
                 var result = await userManager.CreateAsync(person, "putuyoga@GMAIL.com");
                 var newPerson = await userManager.FindByEmailAsync(email);
 

4a7cf796e9af5b5dd0e4978e1642cdc0322ce795
I Putu Yoga Permana <putuyoga@outlook.com>
2016-07-27 13:48:57 +0800

    Add new file for implementation of Identity Manager

---
+++ src/YogaCore/Data/IIdentityManager.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YogaCore.Data
+{
+    public interface IIdentityManager
+    {
+        Task<bool> LoginAsync(string username, string password);
+
+        Task<IdentityResult> RegisterAsync(IdentityUser user, string password);
+
+        Task LogoutAsync();
+    }
+}
---
+++ src/YogaCore/Data/PersonManager.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YogaCore.Models;
+
+namespace YogaCore.Data
+{
+    public class PersonManager : IIdentityManager
+    {
+
+        private readonly SignInManager<Person> _signInManager;
+        private readonly UserManager<Person> _userManager;
+
+        public PersonManager(
+            SignInManager<Person> signInManager, 
+            UserManager<Person> userManager)
+        {
+            _signInManager = signInManager;
+            userManager = _userManager;
+        }
+
+        public async Task<bool> LoginAsync(string username, string password)
+        {
+            var result = await _signInManager.PasswordSignInAsync(username, password, false, lockoutOnFailure: false);
+            return result.Succeeded;
+        }
+
+        public async Task LogoutAsync()
+        {
+            await _signInManager.SignOutAsync();
+        }
+
+        public async Task<IdentityResult> RegisterAsync(IdentityUser user, string password)
+        {
+            return await _userManager.CreateAsync(user as Person, password);
+        }
+    }
+}

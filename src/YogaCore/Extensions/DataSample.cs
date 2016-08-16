afff867a5e48aa8f5597bfc2730968d6c78d849f
I Putu Yoga Permana <putuyoga@outlook.com>
2016-08-16 16:32:37 +0800

    add data sample, to ease first install

---
+++ src/YogaCore/Extensions/DataSample.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using YogaCore.Extensions;
+using System.Threading.Tasks;
+using YogaCore.Models;
+
+namespace YogaCore.Extensions
+{
+    public static class DataSample
+    {
+        public static async Task LoadSample(this IApplicationBuilder app)
+        {
+            var userManager = app.ApplicationServices
+                .GetService(typeof(UserManager<Person>)) as UserManager<Person>;
+            var roleManager = app.ApplicationServices.
+                GetService(typeof(RoleManager<IdentityRole>)) as RoleManager<IdentityRole>;
+
+            // The order is matters
+            // Role first, then user
+            await roleManager.LoadSample();
+            await userManager.LoadSample();
+            
+        }
+    }
+}
